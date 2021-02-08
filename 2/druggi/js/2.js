b51c4c1f27d72784e3610b327db0f77bf6b69b23
Nataša Jovančić <jovancic.natasa@gmail.com>
2021-02-09 00:14:11 +0100

    Add files via upload

---
+++ 2/druggi/js/2.js
@@ -0,0 +1,12 @@
+//decimalni broj se predstavlja kao niz brojeva.Napisati tri funkcije za iracunavanje bitwase and,or,xor dva broja//
+function bitwiseAND(n1, n2) {
+    return n1&n2;
+  }
+  
+  function bitwiseOR(n1, n2) {
+    return n1|n2;
+  }
+  
+  function bitwiseXOR(n1, n2) {
+    return n1^n2;
+  }
